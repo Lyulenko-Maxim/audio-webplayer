ca8bb857cad6d524ef052a1f2452ac4b721a9c70
Maxim <mlyulenko@gmail.com>
2023-09-26 17:43:47 +0300

    [MUSIC] New model: TrackInAlbum

---
+++ backend/src/music/models/track_in_album.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from src.music.models import Track, Album
+
+
+class TrackInAlbum(models.Model):
+    """Модель музыкального трека в альбоме."""
+
+    track = models.OneToOneField(to = Track, on_delete = models.CASCADE, verbose_name = 'Трек')
+    album = models.ForeignKey(to = Album, on_delete = models.CASCADE, verbose_name = 'Альбом')
+
+    class Meta:
+        verbose_name = 'Трек в альбоме'
+        verbose_name_plural = 'Треки в альбомах'
+
+    def __str__(self):
+        return f'{self.album.title} {self.track.title}'
