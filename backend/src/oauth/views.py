d0220d8e7dd112e544cb09102538a9b3093300a8
Maxim <mlyulenko@gmail.com>
2023-09-26 17:56:42 +0300

    [PROJECT] Some fix

--- backend/src/oauth/views.py
+++ backend/src/oauth/views.py
@@ -6,7 +6,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from oauth.permissions import IsAnonymous
+from src.oauth.permissions import IsAnonymous
 from src.oauth.services import JWTService
 from src.oauth.serializers import ArtistSerializer, ListenerSerializer, UserSerializer
 
---
+++ backend/src/users/models/__init__.py
@@ -0,0 +1,4 @@
+from .user import User
+from .artist import Artist
+from .listener import Listener
+from .follower import Follower
